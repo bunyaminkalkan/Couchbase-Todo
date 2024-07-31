738612857fda595a10817ba3dccf72aeaf1c9ff3
bunyaminkalkan <bnkalkan41@gmail.com>
2024-07-31 12:12:58 +0300

    feat: add base router on app.ts

--- auth-service/src/app.ts
+++ auth-service/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { json, urlencoded } from 'body-parser'
+import router from './routers'
 
 
 const app = express()
@@ -7,5 +8,6 @@ const app = express()
 app.use(urlencoded({ extended: false }))
 app.use(json())
 
+app.use('/api', router)
 
 export default app
