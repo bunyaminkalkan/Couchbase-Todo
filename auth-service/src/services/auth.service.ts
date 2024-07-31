18d52ec8da03207006b5e2ed451779cb6b169c1b
bunyaminkalkan <bnkalkan41@gmail.com>
2024-07-31 14:19:35 +0300

    feat: add refresh token function

--- auth-service/src/services/auth.service.ts
+++ auth-service/src/services/auth.service.ts
@@ -1,10 +1,15 @@
 import { getCouchbaseConnection } from "../db";
-import { User } from "../models/user.model";
+import { User } from "../@types";
+import TokenService from "./token.service";
+import bcrypt from "bcryptjs";
+
+const SALT = process.env.SALT || "";
 
 export const register = async (user: User) => {
   const { users } = await getCouchbaseConnection();
-
-  await users.insert(user.id, user);
+  const hashedPassword = await bcrypt.hash(user.password || "", SALT);
+  delete user.password;
+  await users.insert(user.id, { ...user, password: hashedPassword });
 
   const createdUser = await getUserById(user.id);
 
@@ -17,4 +22,45 @@ const getUserById = async (id: string) => {
   const user = await users.get(id);
 
   return user.content;
+};
+
+export const login = async (credentials: {
+  email: string;
+  password: string;
+}) => {
+  const user = await getUserByEmail(credentials.email);
+  if (!bcrypt.compareSync(credentials.password, user.password)) {
+    // throw exception
+    return {
+      success: false,
+    };
+  }
+
+  delete user.password;
+
+  const accessToken = TokenService.generateAccessToken(user);
+  const refreshToken = TokenService.generateRefreshToken({ id: user.id });
+
+  return {
+    accessToken,
+    refreshToken,
+    data: user,
+  };
+};
+
+const getUserByEmail = async (email: string) => {
+  const { cluster } = await getCouchbaseConnection();
+  const result = await cluster.query(
+    "SELECT u.* FROM `UserTodoDB`.`Auth`.`Users` u WHERE email = " +
+      `"${email}"`
+  );
+  return result.rows[0];
+};
+
+export const refreshAccessToken = async (refreshToken: string) => {
+  const verifiedRefreshToken = TokenService.verifyRefreshToken(refreshToken);
+  const user = await getUserById(verifiedRefreshToken.id);
+  delete user.password;
+  const accessToken = TokenService.generateAccessToken(user);
+  return { accessToken };
 };
