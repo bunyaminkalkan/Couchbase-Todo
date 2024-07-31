3ace3a997c93080678c2a1bac06b378a24cd405a
bunyaminkalkan <bnkalkan41@gmail.com>
2024-07-31 14:18:02 +0300

    feat: add refresh token route

--- auth-service/src/routers/auth.router.ts
+++ auth-service/src/routers/auth.router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { signUp } from "../controllers/auth.controller";
+import { signUp, signIn, refreshToken } from "../controllers/auth.controller";
 
 const authRouter = Router();
 
 authRouter.post("/signUp", signUp);
+authRouter.post("/signIn", signIn);
+authRouter.post("/refresh", refreshToken);
 
 export default authRouter;
