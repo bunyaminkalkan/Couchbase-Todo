d47c1a0b27996849472e1f69d15c4ae4c004734d
bunyaminkalkan <bnkalkan41@gmail.com>
2024-07-31 14:17:38 +0300

    feat: add refresh token endpoint

--- auth-service/src/controllers/auth.controller.ts
+++ auth-service/src/controllers/auth.controller.ts
@@ -1,8 +1,32 @@
 import { Request, Response, NextFunction } from "express";
-import { register } from "../services/auth.service";
+import { login, refreshAccessToken, register } from "../services/auth.service";
 
-export const signUp = async (req: Request, res: Response, next: NextFunction) => {
+export const signUp = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const user = req.body;
   const registeredUser = await register(user);
-  res.status(200).json(registeredUser)
+  res.status(200).json(registeredUser);
+};
+
+export const signIn = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const credentials = req.body;
+  const loggedInUser = await login(credentials);
+  res.status(200).json(loggedInUser);
+};
+
+export const refreshToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { refreshToken } = req.body;
+  const newAccessToken = await refreshAccessToken(refreshToken);
+  res.status(200).json(newAccessToken);
 };
