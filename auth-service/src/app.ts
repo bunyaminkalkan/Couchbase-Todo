3c3920ce78126f59d1b97a9acf1bdb00915f150c
bunyaminkalkan <bnkalkan41@gmail.com>
2024-08-01 13:47:22 +0300

    feat: use global error middleware

--- auth-service/src/app.ts
+++ auth-service/src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import { json, urlencoded } from 'body-parser'
 import router from './routers'
-
+import { globalErrorMiddleware } from './middlewares/error.middleware'
 
 const app = express()
 
@@ -9,5 +9,7 @@ app.use(urlencoded({ extended: false }))
 app.use(json())
 
 app.use('/api', router)
+
+app.use(globalErrorMiddleware)
 
 export default app
